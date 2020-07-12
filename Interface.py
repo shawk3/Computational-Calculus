1be2c2bf96fab5227652abc92e52cb3a1a5be9a0
shawk3 <shawk3@outlook.com>
2020-07-11 23:16:06 -0400

    Added added bisection and newtons methods to window

--- Interface.py
+++ Interface.py
@@ -10,6 +10,7 @@ from functools import partial
 
 from Calc import Calc
 from plots import FunctionPlots as fplt
+import numpy as np
 
 
 def Plot(a, b, f):
@@ -32,10 +33,21 @@ def updateCalcFunction(event):
     
 def clear():
     p.clearPlot()
+    
+def bisection(resultlabel, a, b):
+    a = float(a.get())
+    b = float(b.get())
+    result = c.bisection(a,b)
+    resultlabel.config(text = "root: " + str(result))
+    
+def newton(resultlabel, x):
+    x = float(x.get())
+    result = c.newton(x)
+    resultlabel.config(text = "root: " + str(result))
 
 
 root = tk.Tk()
-root.geometry('300x200+100+200')
+root.geometry('250x350+100+200')
 c = Calc(lambda x: x)
 p = fplt(c)
 
@@ -44,29 +56,61 @@ fentry = tk.Entry(root)
 fentry.bind("<FocusOut>", updateCalcFunction)
 # fentry.bind("<Enter>", updateCalcFunction)
 
-a = tk.StringVar()  
-b = tk.StringVar() 
-aentry = tk.Entry(root, textvariable = a)
-bentry = tk.Entry(root, textvariable = b)
-Plotf = partial(Plot, a, b, p.plotf)
+x1 = tk.StringVar()  
+x2 = tk.StringVar() 
+x1entry = tk.Entry(root, textvariable = x1, width = 10)
+x2entry = tk.Entry(root, textvariable = x2, width = 10)
+Plotf = partial(Plot, x1, x2, p.plotf)
 fbutton = tk.Button(root, text = "plot", command = Plotf)
-Plotfp = partial(Plot, a, b, p.plotfp)
+Plotfp = partial(Plot, x1, x2, p.plotfp)
 fpbutton = tk.Button(root, text = "plot derivative", command = Plotfp)
-PlotInt = partial(Plot, a, b, p.plotInt)
+PlotInt = partial(Plot, x1, x2, p.plotInt)
 intbutton = tk.Button(root, text = "plot integral", command = PlotInt)
 clearbutton = tk.Button(root, text = "Clear", command = clear)
+a = tk.StringVar()
+b = tk.StringVar()
+aentry = tk.Entry(root, textvariable = a, width = 10)
+bentry = tk.Entry(root, textvariable = b, width = 10)
+brootlabel = tk.Label(root, text = "root: ")
+bisect = partial(bisection, brootlabel, a, b)
+bisectbutton = tk.Button(root, text = "Run", command = bisect)
+x = tk.StringVar()
+xentry = tk.Entry(root, textvariable = x, width = 10)
+nrootlabel = tk.Label(root, text = "root: ")
+newt = partial(newton, nrootlabel, x)
+newtbutton = tk.Button(root, text = "Run", command = newt)
+
+
+
+
+
+# bisectbutton = tk.Button(root, text = B)
 
 
 flabel.place(x = 5, y = 5)
 fentry.place(x = 100, y = 5)
 tk.Label(root, text = "Xmin").place(x = 5, y = 45)
-aentry.place(x = 45, y = 45)
-bentry.place(x = 45, y = 65)
+x1entry.place(x = 45, y = 45)
+x2entry.place(x = 45, y = 65)
 tk.Label(root, text = "Xmax").place(x = 5, y = 65)
 fbutton.place(x = 180, y = 50)
 fpbutton.place(x = 10, y = 100)
 intbutton.place(x = 10, y = 130)
-clearbutton.place(x=100, y = 150)
+clearbutton.place(x=180, y = 115)
+tk.Label(root, text = "Bisection Method: ").place(x = 10, y = 170)
+tk.Label(root, text = "Left Bound: ").place(x = 5, y = 190)
+tk.Label(root, text = "Right Bound: ").place(x = 5, y = 210)
+aentry.place(x = 85, y = 195)
+bentry.place(x = 85, y = 215)
+bisectbutton.place(x = 180, y = 205)
+brootlabel.place(x = 10, y = 235)
+tk.Label(root, text = "Newton's Method:").place(x=10, y = 260)
+tk.Label(root, text = "Initial Guess: ").place(x = 5, y = 280)
+xentry.place(x = 85, y = 280)
+newtbutton.place(x = 180, y = 280)
+nrootlabel.place(x = 10, y = 300)
+
+
 
 
 
